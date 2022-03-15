bdb37bc3c64967bd77fda5c0c981eb3b6b246cd2
Peterson Macedo <petersondmb@hotmail.com>
2022-03-15 10:08:37 -0300

    frontend portfolio

--- public/js/app.js
+++ public/js/app.js
@@ -37410,8 +37410,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\petersonmv1\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\petersonmv1\resources\sass\app.scss */"./resources/sass/app.scss");
+    __webpack_require__(/*! /home/petersonm/Projects/petersonm/resources/js/app.js */"./resources/js/app.js");
+    module.exports = __webpack_require__(/*! /home/petersonm/Projects/petersonm/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
